62b7af50ac4981c7615911c286c714bb97ba2133
Julio  Perez <60043938+alltall092@users.noreply.github.com>
2023-10-23 18:53:26 -0400

    Delete App-PruebaDeConceptos directory

--- App-PruebaDeConceptos/src/redireccion.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Redirect } from 'react-router-dom';
-
-const Redireccion = () => {
-  return <Redirect to="http://localhost:8000/api/v1/docs/" />;
-};
-
-export default Redireccion;
